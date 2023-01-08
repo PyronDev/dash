fe073663357f2c261e87d622558d3b0e100f0472
Titus M <86983072+titushmeader@users.noreply.github.com>
2023-01-08 12:38:21 +0000

    ad more api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const axios = require("axios");
 
 class DashError extends Error {
 	constructor(message, ...params) {
@@ -17,16 +17,17 @@ class DashError extends Error {
 		return "An unexpected result was recieved, this is usually because the authentication is invalid";
 	}
 	static get InvalidAuthorization () {
-		return "Invalid authorization: check your credentials";
+		return "Invalid authorization: check your credentials or 2FA is enabled";
 	}
 }
 
 async function getCurrentVersion() { return (await axios.get("https://raw.githubusercontent.com/MCMrARM/mc-w10-versiondb/master/versions.json.min")).data.reverse().find(array => array[2] === 0)[0] };
-async function getApi(path, dashClass, headers = undefined) {
-	if (headers === undefined) {
-		headers = {'authorization': `XBL3.0 x=${dashClass.userHash};${dashClass.xstsToken}`,'client-version': await getCurrentVersion(),'user-agent': 'MCPE/UWP',}
-	}
-	try{ return await axios.get(`https://pocket.realms.minecraft.net${path}`, { 'headers': headers }); } catch { throw new DashError(DashError.UnexpectedError);; };
+async function getApi(path, dashClass, customHeaders = {}) {
+	headers = {"authorization": `XBL3.0 x=${dashClass.userHash};${dashClass.xstsToken}`,"client-version": await getCurrentVersion(),"user-agent": "MCPE/UWP",}
+		Object.keys(customHeaders).forEach(key => {
+			headers[key] = customHeaders[key];
+		})
+	try{ return await axios.get(`https://pocket.realms.minecraft.net${path}`, { "headers": headers }); } catch { throw new DashError(DashError.UnexpectedError); };
 }
 class dash {
 	constructor(auth) {
@@ -36,17 +37,18 @@ class dash {
 			}
 			this.userHash = auth.user_hash;
 			this.xstsToken = auth.xsts_token;
-			try { await getApi("", this) } catch { throw new DashError(DashError.InvalidAuthorization); }
+			try { await getApi("/mco/client/compatible", this) } catch { throw new DashError(DashError.InvalidAuthorization); }
 			this.currentVersion = await getCurrentVersion();
 		}) ();
 	}
 
-	async clientCompatible(clientVersion = this.currentVersion) {
-		if (typeof(clientVersion) !== "string") {
+	async clientCompatible(clientVersion = undefined) {
+		if (typeof(clientVersion) !== "string" && clientVersion != undefined) {
 			throw new TypeError(`Unexpected type passed to function`);
 		}
-		clientVersion = await getCurrentVersion();
-		var response = await getApi('/mco/client/compatible', this);
+		if (clientVersion === undefined) { clientVersion = await getCurrentVersion(); };
+		var clientHeaders = {"client-version": clientVersion};
+		var response = await getApi("/mco/client/compatible", this, clientHeaders);
 		switch (response.data) {
 			case "COMPATIBLE":
 				return true;
@@ -57,14 +59,10 @@ class dash {
 		}
 	}
 
-	async getWorlds(clientVersion = this.currentVersion) {
-		if (typeof(clientVersion) !== "string") {
-			throw new TypeError(`Unexpected type passed to function`);
-		}
-		clientVersion = await getCurrentVersion();
-		var response = await getApi('/worlds', this);
+	async getRealms() {
+		var response = await getApi("/worlds", this);
 		if (!response.data.hasOwnProperty("servers")) {
-			throw new DashError(`Invalid property: object does not have a property with the name 'servers'`);
+			throw new DashError(`Invalid property: object does not have a property with the name "servers"`);
 		}
 		try { return response.data["servers"]; } catch { throw new DashError(DashError.UnexpectedResult); }
 	}
