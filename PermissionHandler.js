e323ffa344768d4ff8676d355c94f2ed31e85639
Titus Meader <titusmeader@btinternet.com>
2023-03-23 19:59:45 +0000

    finished i believe

--- PermissionHandler.js
+++
@@ -1,9 +0,0 @@
-const { DashError } = require('./DashError');
-
-async function checkPermission(realm) {
-	if (!realm.owner) {
-		throw new DashError("This method requires you to be owner of the specified realm.");
-	}
-
-}
-module.exports = { checkPermission };
--- VersionHandler.js
+++
@@ -1,5 +0,0 @@
-const fetch = require("node-fetch");
-
-async function getCurrentVersion() { return (await (await fetch("https://raw.githubusercontent.com/MCMrARM/mc-w10-versiondb/master/versions.json.min")).json()).reverse().find(array => array[2] === 0)[0]; };
-
-module.exports = { getCurrentVersion };
